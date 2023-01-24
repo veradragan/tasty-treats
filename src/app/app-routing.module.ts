cb0e5cdb65963b4aa98a9dcb7ed7fac743d0ca46
Vera Dragan <vera.dragan@nagarro.com>
2023-01-24 15:19:47 +0200

    Implement search by name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MealsService } from '../meals.service';
 
 @Component({
   selector: 'app-search',
@@ -6,6 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
- 
+  mealName: string = '';
+
+  searchedMeal: any;
+  ingredients: any[] = [];
+  hasSearched = false;
+  constructor(private mealsService: MealsService) {}
+
   ngOnInit(): void {}
+
+  onSearch() {
+    let meal: any = null;
+    this.mealsService.getMealByName(this.mealName).subscribe({
+      next: (data) => {
+        if (data.meals) meal = data.meals[0];
+
+        if (meal) {
+          console.log(meal.idMeal);
+          this.mealsService.getMealDetails(meal.idMeal).subscribe({
+            next: (data) => {
+              this.searchedMeal = data.meals[0];
+
+              if (this.searchedMeal) {
+                for (let i = 1; i <= 20; i++) {
+                  let ingredient = meal[`strIngredient${i}`];
+                  let measure = meal[`strMeasure${i}`];
+                  if (ingredient) {
+                    this.ingredients.push({ name: ingredient, measure });
+                  }
+                }
+              }
+            },
+          });
+        } else this.searchedMeal = null;
+        this.hasSearched = true;
+      },
+    });
+  }
 }
